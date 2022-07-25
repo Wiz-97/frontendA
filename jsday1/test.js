869a3626a1ac82d9e60c81098305c10d7cba37f1
Wiz-97 <samuel97316@gmail.com>
2022-07-25 16:53:27 +0900

    jsday2

---
+++ jsday1/test.js
@@ -0,0 +1,19 @@
+//주말에 node.js를 설치해 보세요
+//vs code에서 스크립트 실행이 허용이 안 된 경우 powershell에서 설정 변경
+//실행이 되면 ok
+console.log('사과바나나기러기스위스토마토역삼역')
+
+let arr1 = [11, 22, 44, 99, 55]
+        console.log(arr1)
+        console.log(typeof arr1)
+        arr1[2] = 77
+        console.log(arr1.sort())
+        console.log(arr1.reverse())
+        arr1[6] = 44
+        console.log(arr1)
+        arr1=[1,5,6,7];
+
+        let arr2 = ['apple', 'mango', 'kiwi', 'banana', '딸기', '사과']      //인덱스 0~5
+        console.log(arr2)
+        arr2[7]='배'
+        console.log(arr2)
